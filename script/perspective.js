006c4199f9c9f97cb2fac7629cf035dc84fa3fdf
vel <avior557@gmail.com>
2022-11-14 13:32:37 +0200

    email msger

--- script/perspective.js
+++ script/perspective.js
@@ -1,17 +1,25 @@
 // import "./styles.css";
 
-const container = document.querySelector(".container");
-const textWrapper = document.querySelector(".text-wrapper");
-const text = document.querySelector(".text-wrapper h1");
-const textDimensions = textWrapper.getBoundingClientRect();
+// const container = document.querySelector(".container");
+// const wrapper = document.querySelector(".text-wrapper");
+// const text = document.querySelector(".text-wrapper h1");
 
-let screenLog = document.querySelector("#screen-log");
+
 
 // container.addEventListener("mousemove", e => animateOnMouseOver(e));
 // text transforms only if mouse is over it 👇
-textWrapper.addEventListener("mousemove", e => animateOnMouseOver(e));
+
+function perspectiveInit() {
+    const wrappers = document.getElementsByClassName("perspective");
+    for (let i = 0; i < wrappers.length; i++) {
+
+        wrappers[i].addEventListener("mousemove", e => animateOnMouseOver(e));
+    }
+}
 
 function animateOnMouseOver(e) {
+    const textDimensions = e.currentTarget.getBoundingClientRect();
+
     const cursorPositionInsideText = {
         x: e.clientX - Math.round(textDimensions.left),
         y: e.clientY - Math.round(textDimensions.top)
@@ -20,11 +28,11 @@ function animateOnMouseOver(e) {
     const deg = {
         x: 15 * ((cursorPositionInsideText.x / textDimensions.width) * 2 - 1),
         y: 15 * ((cursorPositionInsideText.y / textDimensions.height) * 2 - 1)
-        // if the mouse is inside the textWrapper, this object will have values from -15 to 15
+        // if the mouse is inside the wrapper, this object will have values from -15 to 15
         // try 45
     };
 
-    text.style.transform = `rotateX(${-deg.y}deg)  rotateY(${deg.x}deg)`;
+    e.currentTarget.style.transform = `rotateX(${-deg.y}deg)  rotateY(${deg.x}deg)`;
 
     //   screenLog.innerText = `
     //     X= ${deg.x}
