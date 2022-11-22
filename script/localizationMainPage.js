fad742bbebf926a00e6cd363fd460299ad5ac25a
Vel <avior@gmail.com>
2022-11-23 00:16:22 +0200

    fix bug

---
+++ script/localizationMainPage.js
@@ -0,0 +1,32 @@
+function changeTo(lang) {
+    if (lang=="ua")
+        toUk()
+    if (lang=="ru")
+        toRu()
+
+}
+function toUk() {
+    //menu text
+    localStorage.setItem("lang","ua")
+    document.getElementById("txtMain").innerText = "Головна"
+    document.getElementById("txtPrice").innerText = "Цiни"
+    document.getElementById("txtAboutUs").innerText = "Про нас"
+    document.getElementById("txtReviews").innerText = "Вiдгуки"
+    document.getElementById("txtBlogs").innerText = "Статтi"
+    document.getElementById("txtContacts").innerText = "Контакти"
+
+    //mainPage
+    document.getElementById("txtFPagePrice").innerText = "Цiни"
+    document.getElementById("txtFPageAboutUs").innerText = "Про нас"
+    document.getElementById("txtFPageReviews").innerText = "Вiдгуки"
+    document.getElementById("txtFPageBlogs").innerText = "Статтi"
+    document.getElementById("txtFPageContacts").innerText = "Контакти"
+}
+function toRu() {
+
+}
+
+function initLang() {
+    let lang = localStorage.getItem("lang")
+    changeTo(lang)
+}
