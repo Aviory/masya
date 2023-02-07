2ce5161877f9e27ff6b8797454293f13c019b68d
Vel <avior@gmail.com>
2023-02-07 17:21:55 +0200

    somefix

--- script/mobileMenu.js
+++ script/mobileMenu.js
@@ -4,3 +4,17 @@ function goTo(id) {
     document.getElementById("btnMobileMenu").checked = false
     document.getElementById(id).scrollIntoView();
 }
+
+function goToLoadPage(id) {
+    localStorage.setItem("goTo",id)
+    //todo load index
+}
+
+function goFrom() {
+    let id = localStorage.getItem("goTo")
+    if (id!=="" || id!=null || id != undefined){
+
+        document.getElementById(id).scrollIntoView();
+        localStorage.setItem("goTo","")
+    }
+}
