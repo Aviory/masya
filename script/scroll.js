0dd9d6509fbb7775f840d1722c3353efd56b61a2
Vel <avior@gmail.com>
2023-02-07 10:57:38 +0200

    fix scroll

--- script/scroll.js
+++ script/scroll.js
@@ -1,14 +1,19 @@
 var header_scroll = $('.header_scroll'),
     scrollPrev = 0;
 
+header_scroll.addClass('out');
+
 $(window).scroll(function() {
 
     var scrolled = $(window).scrollTop();
 
-    if ( scrolled > 100 && scrolled > scrollPrev ) {
+    if ( scrolled > 100 && scrolled > scrollPrev || scrolled<700) {
         header_scroll.addClass('out');
     } else {
-        header_scroll.removeClass('out');
+        if (scrolled > 700 )
+            header_scroll.removeClass('out');
     }
+
+
     scrollPrev = scrolled;
 });
